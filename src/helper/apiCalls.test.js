dcaa6d63fcf599030aba6f1c249ca8d8db2a5884
Matthew Renn <rennmatthewp@gmail.com>
2018-02-22 10:37:50 -0700

    adds basic test layout for apiCalls.js

---
+++ src/helper/apiCalls.test.js
@@ -0,0 +1,9 @@
+import { getPokeTypesData } from './apiCalls';
+
+describe('getPokeTypesData', () => {
+  xit('should call fetch with the correct args', () => {});
+
+  xit('should return a resolved promise', () => {});
+
+  xit('should handle a rejected promise', () => {});
+});
