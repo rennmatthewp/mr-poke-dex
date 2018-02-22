c011ce5c52d4ad82a7c680dbdf07e06dad64e34a
Matthew Renn <rennmatthewp@gmail.com>
2018-02-22 10:15:37 -0700

    changes fakeReducer to typesReducer populates redux store with pokeTypes passed in

--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux'
-import fakeReducer from './fake-reducer'
+import { combineReducers } from 'redux';
+import { typesReducer } from './types-reducer';
 
 const rootReducer = combineReducers({
-  fake: fakeReducer
-})
+  types: typesReducer
+});
 
-export default rootReducer
+export default rootReducer;
