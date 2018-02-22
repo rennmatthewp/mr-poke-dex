91c7fac01effd57240cd4cbaf8825096887bcdff
Matthew Renn <rennmatthewp@gmail.com>
2018-02-22 11:08:34 -0700

    adds first fetch test

--- src/helper/apiCalls.test.js
+++ src/helper/apiCalls.test.js
@@ -1,7 +1,19 @@
 import { getPokeTypesData } from './apiCalls';
+import * as mock from '../mock/mockData';
 
 describe('getPokeTypesData', () => {
-  xit('should call fetch with the correct args', () => {});
+  window.fetch = jest.fn().mockImplementationOnce(() =>
+    Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve(mock.pokeTypes)
+    })
+  );
+
+  it('should call fetch with the correct args', () => {
+    getPokeTypesData();
+
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/types');
+  });
 
   xit('should return a resolved promise', () => {});
 
